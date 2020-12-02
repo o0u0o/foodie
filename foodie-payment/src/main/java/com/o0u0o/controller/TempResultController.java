0fc9ed2a4e21de083ff4bf6fbdf5d1824356661a
岳川贵 <jobs@techgeng.com>
2020-12-02 16:39:59 +0800

    :apple: 支付中心模块

---
+++ foodie-payment/src/main/java/com/o0u0o/controller/TempResultController.java
@@ -0,0 +1,19 @@
+package com.o0u0o.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@ApiIgnore
+public class TempResultController {
+
+	@GetMapping("/alipayResult")
+	public String alipayResult(HttpServletRequest request, HttpServletResponse response) {
+
+		return "alipayResult";
+	}
+}
---
+++ foodie-payment/src/main/java/com/o0u0o/service/UserService.java
@@ -0,0 +1,13 @@
+package com.o0u0o.service;
+
+import com.o0u0o.pojo.Users;
+
+public interface UserService {
+
+	/**
+	 * @Description: 查询用户信息
+	 */
+	public Users queryUserInfo(String userId, String pwd);
+
+}
+
