5ef2b0bcda1e85ab778cc8fb3ed643a209753700
岳川贵 <jobs@techgeng.com>
2021-05-19 06:30:26 +0800

    :apple: 缓存穿透

---
+++ foodie-api/src/main/java/com/o0u0o/controller/RedisController.java
@@ -0,0 +1,16 @@
+package com.o0u0o.controller;
+
+import com.o0u0o.utils.RedisOperator;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Redis测试接口
+ * @author mac
+ * @date 2021/1/20 10:12 下午
+ */
+public class RedisController {
+
+    @Autowired
+    private RedisOperator redisOperator;
+
+}
