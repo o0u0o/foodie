5dc3bd73ae2ee4bd27043c9053988bb01f99f78d
岳川贵 <jobs@techgeng.com>
2021-10-20 14:24:16 +0800

    :apple: 更新代码

--- foodie-api/src/main/java/com/o0u0o/controller/RedisController.java
+++ foodie-api/src/main/java/com/o0u0o/controller/RedisController.java
@@ -1,16 +1,43 @@
 package com.o0u0o.controller;
 
-import com.o0u0o.utils.RedisOperator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Redis测试接口
- * @author mac
+ * @author o0u0o
  * @date 2021/1/20 10:12 下午
  */
+@ApiIgnore
+@RestController("redis")
 public class RedisController {
 
     @Autowired
-    private RedisOperator redisOperator;
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/set")
+    public Object set(String key, String value){
+        //设置字符串类型的
+        redisTemplate.opsForValue().set(key, value);
+        return "OK";
+    }
+
+    @GetMapping("/get")
+    public Object get(String key){
+        return  (String)redisTemplate.opsForValue().get(key);
+    }
+
+    @GetMapping("/delete")
+    public Object delete(String key){
+        Boolean delete = redisTemplate.delete(key);
+        if (delete != false){
+            return "delete fail!";
+        }
+        return "delete success!";
+    }
+
 
 }
