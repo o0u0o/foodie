567950c31602b2fb813f75aae94edeb1fca04a80
岳川贵 <jobs@techgeng.com>
2021-10-22 16:01:31 +0800

    :apple: 统一认证管理系统Controller登录

--- foodie-cas/src/main/java/com/o0u0o/controller/CASController.java
+++ foodie-cas/src/main/java/com/o0u0o/controller/CASController.java
@@ -4,6 +4,7 @@ import com.sun.deploy.net.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
