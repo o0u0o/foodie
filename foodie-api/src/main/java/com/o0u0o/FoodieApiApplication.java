fd2c697956d100450f10354c4bb1f4a78579fb58
岳川贵 <jobs@techgeng.com>
2021-10-22 15:48:31 +0800

    :apple: 删除没必要的*.iml

--- foodie-api/src/main/java/com/o0u0o/FoodieApiApplication.java
+++ foodie-api/src/main/java/com/o0u0o/FoodieApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -23,7 +23,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.o0u0o", "org.n3r.idworker"})
 //开始基于redis作为spring session
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 // 开启定时任务
 @EnableScheduling
 public class FoodieApiApplication {
